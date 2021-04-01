1b710222b5aecf9e04febfb02ef327ce12dcb570
swiichy <boomheadshotmn@gmail.com>
2021-04-01 21:29:38 +0200

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Card = (props) => {
+    const { country } = props;
+
+    const numberFormat = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
+    return (
+        <div>
+            <li className="card">
+                <img src={country.flag} alt="flag" />
+                <div className="data-container">
+                    <ul>
+                        <li>{country.name}</li>
+                        <li>{country.capital}</li>
+                        <li>Population: {numberFormat(country.population)}</li>
+                    </ul>
+                </div>
+            </li>
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/pages/About.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+return (
+<div>
+    <Navigation />
+    <Logo />
+    <h1>A propos</h1>
+    <br />
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo ullam cumque odio delectus fuga similique possimus!
+        Est, natus tempore. Enim, cupiditate. Mollitia eligendi, ut sunt reiciendis deleniti rerum vel nemo iure
+        corporis
+        minus odio quidem tempore consectetur in, accusamus asperiores culpa ab, quia molestiae obcaecati. Nobis beatae,
+        fuga molestiae repellendus accusamus sapiente sunt quos officiis aliquid explicabo in cumque quis rerum non
+        totam,
+        delectus, quisquam possimus labore quibusdam? Doloribus a, quia dicta perspiciatis corrupti ex minima
+        consequuntur
+        accusantium, libero placeat vero tempore ipsa dolor nihil eos sit odio? Provident tenetur odio eos nulla quo
+        officia
+        voluptate sequi dolorem repudiandae fugiat. Quaerat dicta vero at ab asperiores ipsa iste? Sunt, molestiae a
+        necessitatibus debitis laudantium quasi doloribus culpa sit minima illo nulla. Nihil sed laborum saepe
+        repudiandae,
+        sunt ipsam placeat hic illo reiciendis velit quidem eligendi distinctio consequuntur porro? Error, libero
+        sapiente
+        veniam cupiditate temporibus illo repellendus reprehenderit facilis similique aliquid, ex neque consequuntur
+        ullam.
+    </p>
+    <br />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nulla modi recusandae sequi. Et ratione cum,
+        deserunt saepe maiores a deleniti laudantium facilis neque, delectus nostrum vero, sequi similique ad magnam?
+        Iure rerum nulla doloremque a laudantium ipsum, dolorum omnis, minima unde officia alias et quas doloribus
+        totam? Suscipit a dicta quos accusamus! Ad maiores officiis, ullam similique tempore commodi voluptatibus dolor
+        nemo, ut eveniet deleniti iste, molestiae dolorum ab voluptatum adipisci ipsam! Hic, iste excepturi! Architecto
+        dolorem dolore earum quia illo tenetur eligendi voluptatum minima laudantium officiis soluta id, omnis
+        voluptates dolorum reprehenderit blanditiis alias nihil, beatae ipsa maxime voluptas nisi cumque corporis.
+        Beatae, sit esse! Error, iusto ea inventore perspiciatis dolore debitis vel eveniet repellendus, omnis nemo
+        molestias ipsa ut aperiam eum numquam suscipit nobis autem maiores quam sunt dolor eaque? Accusantium, dolorem
+        harum iure aliquam expedita sint corporis blanditiis! Explicabo quae ipsam tempora neque quidem architecto,
+        facere voluptas magnam aliquid expedita.
+    </p>
+</div>
+);
+};
+
+export default About;
